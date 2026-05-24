edfcab18ae1224efd516317a2495f988e8f30132
Kumkum kaushik <kumkumkaushik34@gmail.com>
2026-05-24 23:35:58 +0530

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -1,20 +1,27 @@
 import chromadb
 
-client= chromadb.Client()
-collection= client.create_collection("krishna_philosophy")
+client = chromadb.Client()
+
+def get_collection():
+    try:
+        return client.get_collection("krishna_philosophy")
+    except:
+        return client.create_collection("krishna_philosophy")
 
 def store_chunks(chunks, embeddings):
-    ids= [str(i) for i in range(len(chunks))]
+    collection = get_collection()
+    ids = [str(i) for i in range(len(chunks))]
     collection.add(
-        documents= chunks,
-        embeddings= embeddings.tolist(),
-        ids= ids
+        documents=chunks,
+        embeddings=embeddings.tolist(),
+        ids=ids
     )
-    print(f"Stored {len(chunks)} chunks in ChromaDB.")
+    print(f"Stored {len(chunks)} chunks in ChromaDB")
 
 def retrieve(query_embedding, n_results=3):
-    results= collection.query(
-        query_embeddings= query_embedding.tolist(),
-        n_results= n_results
+    collection = get_collection()
+    results = collection.query(
+        query_embeddings=query_embedding.tolist(),
+        n_results=n_results
     )
     return results['documents'][0]
